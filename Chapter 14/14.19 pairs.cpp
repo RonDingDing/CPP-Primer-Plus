4ecf660ffa1afa741d8df68b74a8ef49b18e828f
Ron <411312915@qq.com>
2018-02-26 11:13:30 +0800

    20180226

---
+++ Chapter 14/14.19 pairs.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+template <class T1, class T2>
+class Pair
+{
+  private:
+    T1 a;
+    T2 b;
+
+  public:
+    T1 &first();
+    T2 &second();
+    T1 first() const { return a; }
+    T2 second() const { return b; }
+    Pair(const T1 &aval, const T2 &bval) : a(aval), b(bval) {}
+    Pair() {}
+};
